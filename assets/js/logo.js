f07ac2b4e74da1380f0ba343fac1c2ec0f5388b5
sun_xiaojie <1766681193@qq.com>
2020-08-24 15:37:46 +0800

    完成了注册功能

--- assets/js/logo.js
+++ assets/js/logo.js
@@ -1,3 +1,11 @@
+// ---------------------------切换登录和注册的盒子--------------------
+$('.login a').click(function () {
+    $('.register').show().prev().hide();
+});
+$('.register a').click(function () {
+    $('.login').show().next().hide();
+});
+
 // -----------------完成登录功能----------------------
 $('.login form').on('submit', function (e) {
     e.preventDefault();
@@ -14,6 +22,28 @@ $('.login form').on('submit', function (e) {
                 localStorage.setItem('token', res.token)
                 // 跳转到index.html页面
                 location.href = '/index.html';
+            }
+        }
+    })
+})
+
+// --------------------------注册功能------------------
+// 表单提交，阻止默认行为，收集用户名和密码，用ajax注册接口
+$('.register form').on('submit', function (e) {
+    e.preventDefault();
+    // 收集表单数据
+    var data = $(this).serialize();
+    // console.log(data);
+    $.ajax({
+        type: 'POST',
+        url: 'http://ajax.frontend.itheima.net/api/reguser',
+        data: data,
+        success: function (res) {
+            layer.msg(res.message);
+            if (res.status === 0) {
+                $('.login').show().next().hide();
+                // 清空注册的表单
+                $('.register form')[0].reset();
             }
         }
     })
